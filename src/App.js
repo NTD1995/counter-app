d118378020c650f148ee3e83c14a3bdb82fc2865
Shogo Wada <aiz4338150@gmail.com>
2023-12-12 21:24:36 +0900

    コメント追加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 export default function App() {
+  // 〇〇の状態を管理するステート 
+  // カウンターの状態を管理するステート
   const [count, setCount] = useState(0);
+  
+  // 〇〇を〇〇する処理
+  // カウントの数字を1つ増やす処理
   const handleIncrease = () => {
     setCount((count) => count + 1);
   };
 
+  // 〇〇を〇〇する処理
+  // カウントの数字を1減らす処理
   const handleDecrease = () => {
     setCount((count) => count - 1);
   };
 
+  // 〇〇を〇〇する処理
+  // カウントの数字を2倍にする処理
   const handleDouble = () => {
     setCount((count) => count * 2);
   };
 
+  // 〇〇を〇〇する処理
+  // カウントの数字をリセットする処理
   const handleReset = () => {
     setCount(0);
   };
@@ -34,11 +45,13 @@ export default function App() {
       <button style={{ fontSize: "20px" }} onClick={handleReset}>
         リセット
       </button>
+      {/* 10以上になったとき、 10以上になりました。が表示される*/}
       {count >= 10 && (
         <div style={{ fontSize: "24px", marginTop: "40px" }}>
           10以上になりました。
         </div>
       )}
+      {/* 0以下になったとき、 0以下になりました。が表示される*/}
       {count <= 0 && (
         <div style={{ fontSize: "24px", marginTop: "40px" }}>
           0以下になりました。
